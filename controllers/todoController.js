962196868920fb478df7bec56324f977aaba653c
Melanie Williams <melaniep518@gmail.com>
2020-06-04 20:44:06 -0400

    connect to mongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,12 +1,34 @@
+const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const urlencodedParser = bodyparser.urlencoded({ extended: false });
+const password = encodeURI('p@ssword123');
+const dbname = 'to-do-list';
 
+// DB Configuration
+mongoose.connect(`mongodb+srv://melanie:${password}@to-do-list-cv5ob.mongodb.net/${dbname}?retryWrites=true&w=majority`, { 
+  useUnifiedTopology: true, 
+  useNewUrlParser: true 
+});
+
+const todoSchema = new mongoose.Schema({
+  item: String
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+const itemOne = Todo({item: 'make dinner reservations'}).save(err => {
+  if(err) throw err;
+  console.log('item saved');
+});
+
+// Dummy Data
 let todos = [
   { item: 'laundry' },
   { item: 'walk dog' },
   { item: 'buy groceries' }
 ];
 
+// Router
 const router = (app) => {
   app.get('/todo', getTodos);
   app.post('/todo', urlencodedParser, postTodo);
